ddb754f63652767e57e6351c2700481e1b923212
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2024-01-28 13:42:32 -0800

    Initial commit

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = GetComponent<Transform>().position;
+        // Debug.Log("I'm in start at frame " + Time.frameCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform siblingTransform = GetComponent<Transform>();
+
+        float offset = Mathf.Sin(Time.time);
+        siblingTransform.position = startingPosition + Vector3.right * offset;
+        // Debug.Log("I'm in update at frame " + Time.frameCount);
+    }
+}
